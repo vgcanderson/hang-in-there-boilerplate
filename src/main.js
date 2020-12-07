1ca1add9f81264ffda2c60d765fbd47bf1340330
Chris Anderson <chrisanderson@velocityglobal.com>
2020-12-07 14:17:02 -0700

    commit test.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 // query selector variables go here 👇
+let htmlImage = document.getElementById('motivationalImage')
+let posterTitle = document.querySelector('h1')
+let quote = document.querySelector('h3')
+let randomPosterButton = document.querySelector('.show-random')
+let makePosterButton = document.querySelector('.show-form')
+let returnToMainButton = document.querySelector('.show-main')
+let backToMainButton = document.querySelector('.back-to-main')
+let savedPostersButton = document.querySelector('.show-saved')
+let savePosterButton = document.querySelector('.save-poster')
+// let createCustomPosterButton = document.querySelector('.make-poster')
 
-// we've provided you with some data to work with 👇
-var images = [
+let images = [
   "./assets/bees.jpg",
   "./assets/bridge.jpg",
   "./assets/butterfly.jpg",
@@ -21,7 +30,7 @@ var images = [
   "./assets/tiger.jpg",
   "./assets/turtle.jpg"
 ];
-var titles = [
+let titles = [
   "determination",
   "success",
   "inspiration",
@@ -58,7 +67,7 @@ var titles = [
   "understanding",
   "wisdom"
 ];
-var quotes = [
+let quotes = [
   "Don’t downgrade your dream just to fit your reality, upgrade your conviction to match your destiny.",
   "You are braver than you believe, stronger than you seem and smarter than you think.",
   "You are confined only by the walls you build yourself.",
@@ -98,14 +107,95 @@ var quotes = [
   "Each person must live their life as a model for others.",
   "A champion is defined not by their wins but by how they can recover when they fall."
 ];
-var savedPosters = [];
-var currentPoster;
-
-// event listeners go here 👇
+let savedPosters = [];
+let currentPoster = [];
 
 // functions and event handlers go here 👇
-// (we've provided one for you to get you started)!
+createPoster()
+
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
 }
+
+function createPoster() {
+  //Create a poster and overwrite contents of 'currentPoster' with the image, title, and quote
+  console.log('createPoster function')
+  htmlImage.src = images[getRandomIndex(images)]
+  currentPoster.splice(0, 1, htmlImage.src)
+  posterTitle.innerText = titles[getRandomIndex(titles)]
+  currentPoster.splice(1, 1, posterTitle.innerText)
+  quote.innerText = quotes[getRandomIndex(quotes)]
+  currentPoster.splice(2, 1, quote.innerText)
+}
+
+function showForm() {
+  console.log('showForm function')
+  document.querySelector('.main-poster').classList.add('hidden')
+  document.querySelector('.poster-form').classList.remove('hidden')
+}
+
+function backToMain() {
+  console.log('backToMan function')
+  //Hide the Form, create a new poster, then show the main page
+  document.querySelector('.poster-form').classList.add('hidden')
+  document.querySelector('.saved-posters').classList.add('hidden')
+  createPoster()
+  document.querySelector('.main-poster').classList.remove('hidden')
+}
+
+function showSavedPosters() {
+  console.log('showSavedPoster function')
+  document.querySelector('.main-poster').classList.add('hidden')
+  //forEach loop to add all posters to page
+  savedPosters.forEach((posterObject) => {
+    //Check if the element already exists
+    if (document.getElementById(posterObject.id) === null) {
+      //Create a new mini-poster element, a new img element, a new h2 element, and a new h4 element
+      let newMiniPoster = document.createElement('mini-poster')
+      let miniImage = document.createElement('img')
+      miniImage.src = posterObject.imageURL
+      let miniH2 = document.createElement('h2')
+      miniH2.innerText = posterObject.title
+      let miniH4 = document.createElement('h4')
+      miniH4.innerText = posterObject.quote
+      // //Assign an ID to the mini-poster element, add the classList to it, then append the image, h2, and h4
+      newMiniPoster.id = posterObject.id
+      newMiniPoster.classList.add('mini-poster')
+      newMiniPoster.appendChild(miniImage)
+      newMiniPoster.appendChild(miniH2)
+      newMiniPoster.appendChild(miniH4)
+      //add the mini-poster to the DOM
+      let miniPostersElement = document.getElementById('postersGrid')
+      miniPostersElement.appendChild(newMiniPoster)
+    }
+  })
+  document.querySelector('.saved-posters').classList.remove('hidden')
+}
+
+function savePoster() {
+  console.log('savePoster function')
+  // Create a new Poster object and push into 'savedPosters'
+  savedPosters.push(new Poster(currentPoster[0], currentPoster[1], currentPoster[2]))
+}
+
+// function createCustomPoster() {
+//   console.log('createCustomPoster function')
+//   htmlImage.src = document.getElementById('poster-image-url').value
+//   currentPoster.splice(0, 1, htmlImage.src)
+//   posterTitle.innerText = document.getElementById('poster-title').value
+//   currentPoster.splice(1, 1, posterTitle.innerText)
+//   quote.innerText = document.getElementById('poster-quote').value
+//   currentPoster.splice(2, 1, quote.innerText)
+//   // savePoster()
+//   document.querySelector('.poster-form').classList.add('hidden')
+//   document.querySelector('.main-poster').classList.remove('hidden')
+// }
+
+randomPosterButton.addEventListener('click', createPoster)
+makePosterButton.addEventListener('click', showForm)
+returnToMainButton.addEventListener('click', backToMain)
+savedPostersButton.addEventListener('click', showSavedPosters)
+backToMainButton.addEventListener('click', backToMain)
+savePosterButton.addEventListener('click', savePoster)
+// createCustomPosterButton.addEventListener('click', createCustomPoster)
 
